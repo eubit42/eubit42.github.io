851429b6eabd96afdb2fb43385d84194c9462162
eubit42 <72103166+eubit42@users.noreply.github.com>
2020-11-15 18:43:18 +0100

    Add files via upload

---
+++ HamdiHamadAssignment6/js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  $(document).foundation();
+})
+
+var elem = new Foundation.ResponsiveToggle(element, options);
+
+var elem = new Foundation.DropdownMenu(element, options);
+
+var elem = new Foundation.AccordionMenu(element, options);
+
+var elem = new Foundation.OffCanvas(element, options);
+
+var elem = new Foundation.Orbit(element, options);
+
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+
+var password = document.getElementById("new_psswd")
+  , confirm_password = document.getElementById("confirm_psswd");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+function CheckPassword(inputtxt) 
+{ 
+  var decimal=  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,}$/;
+  if(inputtxt.value.match(decimal)) 
+  { 
+    return true;
+  }
+  else
+  { 
+    alert('Password must be atleast 8 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character')
+    return false;
+  }
+} 
+
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
